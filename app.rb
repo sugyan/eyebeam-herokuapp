1aff016e370ebad8fd331365c298a309d79400b7
sugyan <sugi1982@gmail.com>
2012-06-04 08:36:07 +0900

    add eye beam

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'blitline'
 require 'dalli'
 require 'haml'
 require 'json'
+require 'open-uri'
+require 'rexml/document'
 require 'sinatra'
 
 enable :logging
@@ -27,9 +29,18 @@ get '/image' do
   end
 end
 
+get '/api/face' do
+  if url = params[:url]
+    data = kaolabo(url)
+    data
+  else
+    error 400, 'Bad Request'
+  end
+end
+
 def blitline (url)
-  sha1 = Digest::SHA1.hexdigest(url)
-  if cached = settings.cache.get(sha1)
+  key = 'blitline:' + Digest::SHA1.hexdigest(url)
+  if cached = settings.cache.get(key)
     return cached
   else
     job = Blitline::Job.new(url)
@@ -40,7 +51,37 @@ def blitline (url)
     blitline.jobs << job
     json = blitline.post_jobs
     logger.info json
-    settings.cache.set(sha1, json)
+    settings.cache.set(key, json)
     return json
+  end
+end
+
+def kaolabo (url)
+  key = 'kaolabo:' + Digest::SHA1.hexdigest(url)
+  if cached = settings.cache.get(key)
+    return cached
+  else
+    uri = "https://kaolabo.com/api/detect?apikey=#{ ENV['KAOLABO_APIKEY'] }&url=#{ URI.encode(url) }"
+    doc = REXML::Document.new(open(uri).read)
+    face = doc.elements['results/faces[1]/face']
+    data = {
+      :face => {
+        :height => face.attributes['height'],
+        :width  => face.attributes['width'],
+        :x      => face.attributes['x'],
+        :y      => face.attributes['y'],
+      },
+      :left_eye => {
+        :x => face.elements['left-eye'].attributes['x'],
+        :y => face.elements['left-eye'].attributes['y'],
+      },
+      :right_eye => {
+        :x => face.elements['right-eye'].attributes['x'],
+        :y => face.elements['right-eye'].attributes['y'],
+      }
+    }.to_json
+    logger.info data
+    settings.cache.set(key, data)
+    return data
   end
 end
--- public/js/main.js
+++ public/js/main.js
@@ -9,8 +9,22 @@
                 }).hide();
                 img.load(function () {
                     $('#message').remove();
-                    var paper = Raphael('image', img.width(), img.height());
-                    var image = paper.image(url, 0, 0, img.width(), img.height());
+                    var width  = img.width();
+                    var height = img.height();
+                    var paper = Raphael('image', width, height);
+                    var image = paper.image(url, 0, 0, width, height);
+                    $.ajax({
+                        url: '/api/face',
+                        data: { url: url },
+                        dataType: 'json',
+                        success: function (res) {
+                            var r = paper.path('M' + res.right_eye.x + ',' + res.right_eye.y + 'L' + res.right_eye.x + ',' + height);
+                            var l = paper.path('M' + res.left_eye.x  + ',' + res.left_eye.y  + 'L' + res.left_eye.x  + ',' + height);
+                            r.attr('stroke', 'white');
+                            l.attr('stroke', 'white');
+                        }
+                    });
+                    img.remove();
                 });
                 img.error(function () {
                     img.remove();
