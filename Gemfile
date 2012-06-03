c8afef4e81a67e10ab1598349584258dd48a39d8
sugyan <sugi1982@gmail.com>
2012-06-03 11:10:51 +0900

    use blitline

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
-gem 'sinatra', '>=1.3.2'
-gem 'thin',    '>=1.3.1'
+gem 'sinatra',  '>=1.3.2'
+gem 'thin',     '>=1.3.1'
+gem 'blitline', '>=1.3.0'
--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'blitline'
 
 get '/' do
-  'Hello, world!'
+  job = Blitline::Job.new(params[:url])
+  job.application_id = ENV['BLITLINE_APPLICATION_ID']
+  function = job.add_function('resize_to_fit', { :width  => 100, :height => 100 })
+  function.add_save('eyebeam')
+  blitline = Blitline.new
+  blitline.jobs << job
+  blitline.post_jobs
 end
